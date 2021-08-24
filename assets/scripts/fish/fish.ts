b33ce53d1e23fe9e2d0aa9f95e94be900adcdd19
Gia Bao <bao.ntg@gemi.io>
2021-08-24 19:06:05 +0700

    Move fishes

--- assets/scripts/fish/fish.ts
+++ assets/scripts/fish/fish.ts
@@ -1,6 +1,8 @@
 
 import { _decorator, Component, Node, SkeletalAnimationComponent, AnimationClip, Vec3 } from 'cc';
+import Random from '../utils/random';
 const { ccclass, property } = _decorator;
+type Range = { left: number, right: number, top: number, bot: number };
 
 @ccclass('Fish')
 export class Fish extends Component {
@@ -14,27 +16,71 @@ export class Fish extends Component {
     private nodeModel: Node = null;
 
     @property
+    private moveSpeed = 0.0002;
+
+    @property
     private swimAnchor = new Vec3();
 
     private defaultPosition = new Vec3();
+    private movingRange: Range;
+    private isSwimming = false;
+    private posTarget = new Vec3();
 
-    onEnable() {
-        this.swim();
-    }
-
-    onDisable(){
+    onDisable() {
         this.idle();
     }
 
+    update(dt: number) {
+        if (this.isSwimming) {
+            let pos = this.node.getPosition();
+            if (!(pos.x.toFixed(2) != this.posTarget.x.toFixed(2) ||
+                pos.z.toFixed(2) != this.posTarget.z.toFixed(2))) {
+                this.moveNext();
+            } else {
+                let offset = new Vec3();
+                Vec3.subtract(offset, this.posTarget, this.node.position);
+                offset.normalize();
+
+                Vec3.multiplyScalar(offset, offset, this.moveSpeed);
+                let pos = this.node.worldPosition;
+                offset.add(pos);
+                this.node.setPosition(offset);
+            }
+        }
+    }
+
+    setMovingRange(left: number, right: number, top: number, bot: number) {
+        this.movingRange = { left: left, right: right, top: top, bot: bot };
+    }
+
     swim() {
+        this.isSwimming = true;
         this.defaultPosition = this.nodeModel.getPosition();
         this.animation.play(this.clipSwim.name);
         this.nodeModel.setPosition(this.swimAnchor);
+        this.moveNext();
     }
 
-    idle(){
+    idle() {
+        this.isSwimming = false;
         this.animation.stop();
         this.nodeModel.setPosition(this.defaultPosition);
         this.defaultPosition = null;
+    }
+
+    moveNext() {
+        this.posTarget = new Vec3(Random.range(this.movingRange.left, this.movingRange.right),
+            Random.range(-0.02, 0.02),
+            Random.range(this.movingRange.top, this.movingRange.bot));
+        let pos = this.node.position;
+        let targetDir = this.posTarget.clone();
+        targetDir.subtract(pos);
+        let vectorForward = pos.clone();
+        vectorForward.subtract(new Vec3(pos.x, pos.y, -1));
+        let angle = Vec3.angle(targetDir, vectorForward) * 180 / Math.PI;
+        if (targetDir.x < 0) {
+            angle = 360 - angle;
+        }
+        this.node.setRotationFromEuler(new Vec3(0, angle, 0));
     }
 }
